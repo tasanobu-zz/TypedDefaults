26162dd463377a84bc7a9a374fcbb3266a1f6d8e
Kazunobu Tasaka <tasanobu@gmail.com>
2016-03-15 09:39:18 +0900

    Create the Xcode project

---
+++ TypedDefaults/TypedDefaults.h
@@ -0,0 +1,19 @@
+//
+//  TypedDefaults.h
+//  TypedDefaults
+//
+//  Created by Kazunobu Tasaka on 3/15/16.
+//  Copyright © 2016 Kazunobu Tasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TypedDefaults.
+FOUNDATION_EXPORT double TypedDefaultsVersionNumber;
+
+//! Project version string for TypedDefaults.
+FOUNDATION_EXPORT const unsigned char TypedDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TypedDefaults/PublicHeader.h>
+
+
