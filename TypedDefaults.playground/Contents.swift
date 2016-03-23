40d40f44e03f1e7a2aadcb96fc238ceeab422421
Kazunobu Tasaka <tasanobu@gmail.com>
2016-03-23 14:11:12 +0900

    Renew sample code for playgrounds

--- TypedDefaults.playground/Contents.swift
+++ TypedDefaults.playground/Contents.swift
@@ -2,31 +2,55 @@
 
 import TypedDefaults
 
-struct Person: UserDefaultsConvertible {
-    static var key = "Person"
+enum Preference: String {
+    case CameraConfig
     
-    let name: String
-    
-    init?(_ object: AnyObject) {
-        guard let dict = object as? [String: String], let name = dict["name"] else {
-            return nil
-        }
-        self.name = name
-    }
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func serialize() -> AnyObject {
-        return ["name": name]
+    var key: String {
+        let prefix = NSBundle.mainBundle().bundleIdentifier!
+        
+        return prefix + "." + self.rawValue
     }
 }
 
-let person = Person(name: "messi")
+struct CameraConfig: DefaultConvertible {
+    enum Size: Int {
+        case Large, Medium, Small
+    }
+    
+    static let key = Preference.CameraConfig.key
 
-let udw = UserDefaultsWrapper(type: Person.self)
+    let saveToCameraRoll: Bool
+    let size: Size
 
-udw.setObject(person)
-udw.getObject()
-udw.removeObject()
+    init?(_ object: AnyObject) {
+        guard let dict = object as? [String: AnyObject] else { return nil }
+
+        self.saveToCameraRoll = dict["cameraRoll"] as? Bool ?? true
+        if let rawSize = dict["size"] as? Int, let size = Size(rawValue: rawSize) {
+            self.size = size
+        } else {
+            self.size = .Medium
+        }
+    }
+
+    func serialize() -> AnyObject {
+        return ["cameraRoll": saveToCameraRoll, "size": size.rawValue]
+    }
+}
+
+let memoryStore = InMemoryStore<CameraConfig>()
+let anyStore = AnyStore(memoryStore)
+
+/// set a default CameraConfig
+anyStore.set(CameraConfig([:])!)
+
+/// get CameraConfig
+anyStore.get()?.saveToCameraRoll /// true
+anyStore.get()?.size             /// Medium
+
+/// remove CameraConfig
+anyStore.remove()
+anyStore.get()                   /// nil
+
+
+
